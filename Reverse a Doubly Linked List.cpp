37f8914706fe0bbe0fe7dd1d21e3eff42f4a553b
samarthcode1 <nagsamarth3@gmail.com>
2023-10-11 16:50:53 +0530

    commit

---
+++ Reverse a Doubly Linked List.cpp
@@ -0,0 +1,32 @@
+/*
+struct Node
+{
+    int data;
+    Node * next;
+    Node * prev;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+        prev=NULL;
+    }
+        
+};
+*/
+Node* reverseDLL(Node * head)
+{
+    //Your code here
+    Node* temp = NULL;
+    Node* current = head;
+    while (current != NULL) {
+        temp = current->prev;
+        current->prev = current->next;
+        current->next = temp;
+        current = current->prev;
+    }
+    
+    if (temp != NULL){
+        head = temp->prev;
+    }
+    return head;
+}
